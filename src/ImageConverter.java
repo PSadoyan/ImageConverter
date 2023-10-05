8dc6a45c6eed2ed547a17f37eb25b6bac6485593
PSadoyan <pavelsadoyan@gmail.com>
2023-10-04 23:09:59 -0500

    Added Javadoc Documentation

--- src/ImageConverter.java
+++ src/ImageConverter.java
@@ -10,12 +10,28 @@ import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 import java.awt.Dimension;
 
+/**
+ * ImageConverter is a simple Java program that allows you to read an image file,
+ * convert it to a different format, and display it in a JFrame.
+ */
 public class ImageConverter {
-
-	static Scanner input = new Scanner(System.in);
 	
+	/**
+	 * A scanner that takes in relevant filepath and output information.
+	 */
+	static Scanner input = new Scanner(System.in);
+
+	/**
+     * The main method of the ImageConverter program.
+	 * Takes in a Windows file path, reads it as an image,
+	 * writes it as a new image with the desired output image type,
+	 * and opens the file.
+     *
+     * @param args Command-line arguments (replaced functionally by System.in).
+     * @throws Exception If an error occurs during execution.
+     */
 	public static void main(String[] args) throws Exception {
-		System.out.println("Please provide a Windows File Path for the file you wish to convert Below:");
+		System.out.println("Please provide a Windows File Path for the file you wish to convert below:");
 		
 		String in = input.next();
 		in = stripQuotations(in);
@@ -27,6 +43,13 @@ public class ImageConverter {
 		openFile(pic);
 	}
 
+	 /**
+     * Reads an image file and returns it as a BufferedImage.
+     *
+     * @param image The BufferedImage to read the image into.
+     * @param systemInput The file path of the image to be read.
+     * @return The BufferedImage containing the image data.
+     */
 	public static BufferedImage readFile(BufferedImage image, String systemInput) {
 		try {
 			File sampleFile = new File(systemInput);
@@ -38,6 +61,12 @@ public class ImageConverter {
 		return image;
 	}
 
+	/**
+     * Writes a BufferedImage to an output file in the specified image format.
+     *
+     * @param image The BufferedImage to be saved.
+     * @param systemInput The file path of the input image (used to determine the output file type).
+     */
 	public static void writeFile(BufferedImage image, String systemInput) {
 		System.out.println("Please select output location: ");
 		
@@ -51,7 +80,7 @@ public class ImageConverter {
 		
 		
 		try {
-			File output = new File(fileLocation + "\\" + "OpeningImageOutput." + option);
+			File output = new File(fileLocation + "\\" + "ImageConverterOutput." + option);
 			ImageIO.write(image, findingType[findingType.length-1], output);
 			System.out.println("Writing Completed.");
 		} catch(IOException ex) {
@@ -59,8 +88,16 @@ public class ImageConverter {
 		}
 	}
 
+	/**
+     * Opens the converted image in a JFrame for display.
+	 * 
+     * The original point of this project was to open an image using java code, 
+	 * which is why I kept this function, though it is unnecessary.
+	 * 
+     * @param pic The BufferedImage to be displayed.
+     */
 	public static void openFile(BufferedImage pic) {
-		JFrame open = new JFrame("OpeningImage");
+		JFrame open = new JFrame("OpeningImage"); //creates new JFrame to dispaly image
 		open.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		ImageIcon imageicon = new ImageIcon(pic);
@@ -79,9 +116,15 @@ public class ImageConverter {
 		System.out.println("Height: " + imageicon.getIconHeight());
 	}
 
+	 /**
+     * Strips quotation marks from the provided string. 
+     *
+     * @param in The input string that may have leading and trailing quotation marks.
+     * @return The input string with leading and trailing quotation marks removed.
+     */
 	public static String stripQuotations(String in) {
 		if(in.charAt(0) == '\"') {
-			in = in.substring(1,in.length()-1);
+			in = in.substring(1,in.length()-1); //removes quotations from first index and last.
 		}
 		return in;
 	}
